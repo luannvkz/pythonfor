d24bf208929a48fbfe96fe4c6bfc1cdca3c624c3
LUAN DE TOLEDO SANTOS INACIO <luan.inacio@portalsesisp.org.br>
2026-06-11 13:56:09 -0300

    mudança no for

--- atividade.py
+++ atividade.py
@@ -1,31 +1,18 @@
 vendedores = int(input("Quantidade de vendedores: "))
 dias = int(input("Quantidade de dias: "))
 
-total_equipe = 0
-
 for i in range(vendedores):
 
-    nome = input("Nome do vendedor: ")
-
-    total_vendas = 0
-    folgas = 0
+    nome = input("Nome: ")
+    total = 0
 
     for j in range(dias):
 
-        venda = float(input("Valor vendido: "))
+        venda = float(input("Venda: "))
+        total = total + venda
 
-        if venda == 0:
-            folgas = folgas + 1
-
-        else:
-            total_vendas = total_vendas + venda
-
-    media = total_vendas / dias
+    media = total / dias
 
     print("Nome:", nome)
-    print("Total vendido:", total_vendas)
+    print("Total:", total)
     print("Media:", media)
-
-    total_equipe = total_equipe + total_vendas
-
-print("Total da equipe:", total_equipe)
